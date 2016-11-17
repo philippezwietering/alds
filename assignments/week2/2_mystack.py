e9e392385d3ee9bf0a2b74c9be4d99cffde192e6
philippezwietering <philippe.zwietering@gmail.com>
2016-11-17 10:12:18 +0100

    mystack assignment done

---
+++ assignments/week2/2_mystack.py
@@ -0,0 +1,42 @@
+class mystack():
+    stack = []
+    numberOfEntries = 0
+
+    def __init__(self, initialStack = []):
+        self.stack = initialStack
+        self.numberOfEntries = len(initialStack)
+
+    def __repr__(self):
+        return "Size: " + str(self.numberOfEntries) + "\nStack: " + str(self.stack)
+
+    def push(self, n):
+        self.stack.append(n)
+        self.numberOfEntries += 1
+
+    def pop(self):
+        result = self.stack[0]
+        self.stack = self.stack[1:]
+        self.numberOfEntries -= 1
+        return result
+
+    def peek(self, n):
+        assert 0 < n < self.numberOfEntries+1
+        return self.stack[n+1]
+
+    def isEmpty(self):
+        return self.numberOfEntries > 0
+
+
+s = mystack()
+print(s)
+print(s.isEmpty())
+
+for i in range(5):
+    s.push(i)
+print(s)
+
+for i in range(3):
+    print(s.pop())
+
+print(s)
+print(s.isEmpty())
