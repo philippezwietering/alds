d6520376038be71b06ba2d8bf00f6ecc19aa45ea
philippezwietering <philippe.zwietering@gmail.com>
2016-11-17 10:43:07 +0100

    Bracket assignment done

---
+++ assignments/week2/3_bracket_problem.py
@@ -0,0 +1,22 @@
+def checkBrackets(string):
+    stack = []
+    for c in string:
+        assert c in "<>[]()" and len(string) > 1
+
+    for c in string:
+        if c in "<[(":
+            stack.append(c)
+        else:
+            if c == '>' and stack.pop() != '<':
+                return False
+            elif c == ']' and stack.pop() != '[':
+                return False
+            elif c == ')' and stack.pop() != '(':
+                return False
+            # else:
+            #     stack.pop()
+        print(stack)
+    return len(stack) == 0
+
+print(checkBrackets("((<>))"))
+print(checkBrackets("([)]"))
